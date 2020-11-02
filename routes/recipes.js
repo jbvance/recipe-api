ba8f1a280fc12ad05730596a53d8845939557199
Jason Vance <jbvance@gmail.com>
2020-11-01 19:06:49 -0600

    initial commit

---
+++ routes/recipes.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios');
+
+// middleware that is specific to this router
+router.use(function timeLog(req, res, next) {
+  console.log('Time: ', Date.now());
+  next();
+});
+
+router.get('/search', async  (req, res) => {
+  //`https://api.edamam.com/search?q=beef&diet=high-protein&diet=low-carb&diet=low-fat&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}&from=0&to=60&time=1-120&format=json`;
+  const { q } = req.query;
+  const url = encodeURI(`https://api.edamam.com/search?q=${q}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}&from=0&to=60&time=1-120&format=json`);
+  console.log('URL', url);
+  
+  try {
+    const response = await axios.get(url);
+    res.status(200).send(response.data)
+
+  } catch(err) {
+    console.log(err);
+  }  
+});
+
+module.exports = router;
